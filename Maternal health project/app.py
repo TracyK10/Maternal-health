a5156630eca0b5316024f3b173f9d98a537831ae
Tracy Wanjiru Karanja <107494362+TracyK10@users.noreply.github.com>
2023-08-28 14:15:22 +0300

    Add files via upload

---
+++ Maternal health project/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+import africastalking
+
+app = Flask(__name__)
+
+# Configure Africa's Talking credentials
+africastalking_username = "well_mother"
+africastalking_api_key = "0c8f2b254cf1a1bb8d9150a0817fb4fc25485d87f56eacb856df74847c6af4fb"
+
+africastalking.initialize(africastalking_username, africastalking_api_key)
+sms = africastalking.SMS
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send_sms', methods=['POST', 'GET'])
+def send_sms():
+    recipient_phone = request.form.get('recipient_phone')
+    message = "Welcome to Well Mother. We are glad you have joined us. This is how you will be receiving daily updates on maternal health."
+
+    try:
+        # Send SMS using Africa's Talking API
+        response = sms.send(message, [recipient_phone])
+        return f"SMS sent successfully to {recipient_phone}. Message ID: {response['SMSMessageData']['Message']['Id']}"
+    except Exception as e:
+        return f"Failed to send SMS: {str(e)}"
+
+@app.route('/static/css/<path:filename>')
+def css_file(filename):
+    return send_from_directory('static/css', filename), 200, {'Content-Type': 'text/css'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
